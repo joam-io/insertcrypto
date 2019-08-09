39caf55a5ca6903605d5dda11cf248883737ab98
JB <jose.brey@arivanza.com>
2019-08-09 10:35:59 +0200

    Removed Strings lib deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,32 +1,25 @@
 require('openzeppelin-test-helpers/configure')({ web3 });
 const { singletons } = require('openzeppelin-test-helpers');
 
-//const SimpleStorage = artifacts.require('./SimpleStorage.sol');
-
 const SafeMath = artifacts.require('SafeMath.sol');
 
-const Strings = artifacts.require('./Strings.sol');
 const CryptoArcade = artifacts.require('./CryptoArcade.sol');
 const CryptoArcadeGame = artifacts.require('./CryptoArcadeGame.sol');
 const RewardSplitter = artifacts.require('./RewardSplitter.sol');
-//const Zenny = artifacts.require('./Zenny.sol');
+const Zenny = artifacts.require('./Zenny.sol');
 
 const totalSupply = 80000000;
 
 const game1 = {
 	name: 'Reacteroids',
 	creator: '0x522fe0d2f80f7877f940Eab0c95F4B535c8d8797',
-	price: 4810000000000000
+	price: 9000000000000000
 };
 
 const arcadeOperator = '0xDd0f1B9a4064460A4522e37B5Af46dE814F75bED';
 
 module.exports = async function(deployer, network, accounts) {
-	// await deployer.deploy(SimpleStorage);
-	await deployer.deploy(Strings);
 	await deployer.deploy(RewardSplitter);
-	await deployer.link(Strings, CryptoArcade);
-	await deployer.link(Strings, CryptoArcadeGame);
 	await deployer.deploy(
 		CryptoArcadeGame,
 		game1.name,
@@ -40,5 +33,5 @@ module.exports = async function(deployer, network, accounts) {
 		await deployer.deploy(SafeMath);
 		await singletons.ERC1820Registry(accounts[0]);
 	}
-	// deployer.deploy(Zenny, totalSupply);
+	deployer.deploy(Zenny, totalSupply);
 };
